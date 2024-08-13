7047f099da857dfba304ea7c549aeff792fdbf7a
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-08-13 17:36:54 -0500

    stuff

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './css/index.css'
 import Home from './tsx/pages/Home.tsx'
 import Error from './tsx/pages/Error.tsx'
-import NavbarWrapper from './tsx/components/NavbarWrapper.tsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <NavbarWrapper />,
+    element: <Home/>,
     errorElement: <Error />,
-    children: [
-      {
-        path: '/', // yes, again
-        element: <Home />,
-        errorElement: <Error />,
-      }
-    ],
   },
 ]);
 
--- src/tsx/components/Navbar.tsx
+++
@@ -1,5 +0,0 @@
-import 'react'
-
-export default function Navbar() {
-    return <div>navbar</div>
-}
