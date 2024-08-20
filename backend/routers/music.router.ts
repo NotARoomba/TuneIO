964d1040102d5b9fc2d7675df6185ec6946eed17
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-08-19 20:28:34 -0500

    Update music.router.ts

--- backend/routers/music.router.ts
+++ backend/routers/music.router.ts
@@ -96,7 +96,7 @@ const refreshDaily = async () => {
       const cutStream = new Writable();
       ffmpeg(stream)
   .setStartTime((Math.random() * (search[0].duration-20))+10)
-  .setDuration(10)
+  .setDuration(10).withNoVideo().toFormat("wav")
   .output(cutStream)
   .on('end', function(err) {
     if(!err) { console.log('Conversion Done') }
